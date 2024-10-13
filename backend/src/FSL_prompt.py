ecd3f7480fd1ee8dd7cbc3f987321911f13faf71
huy <htang35@mylangara.ca>
2024-10-13 01:03:14 -0700

    Apply software practices and add documentation

--- backend/src/FSL_prompt.py
+++ backend/src/FSL_prompt.py
@@ -4,76 +4,138 @@ from langchain_core.prompts import FewShotChatMessagePromptTemplate, ChatPromptT
 from langchain_core.messages import SystemMessage
 from langchain_openai import OpenAIEmbeddings
 
-examples = [
-    {
-        "Question": "Total population in a random place",
-        "SQLQuery": "SELECT COUNT(*) FROM customers WHERE perfume_purchased = 'true'",
-        "SQLResult": "12345"
-    }
-]
+from data_loader import load_message_from_file, load_json_from_file
+from config import DAtA_DIR
 
-to_vectorize = [' '.join(example.values()) for example in examples]
-embeddings = OpenAIEmbeddings()
-vectorstore = Chroma.from_texts(to_vectorize, embeddings, metadatas=examples)
+from typing import List, Dict
 
-class CustomSemanticSimilarityExampleSelector(SemanticSimilarityExampleSelector):
-    similarity_threshold: float = 0.8
 
-    def select_examples(self, input_variables):
+def create_vectorstore(examples: List[Dict[str, str]]) -> Chroma:
+    """
+    Creates a vector store from the provided examples for semantic similarity search.
+
+    Args:
+        examples (List[Dict[str, str]]): List of example dictionaries containing 'Question', 'SQLQuery', and 'SQLResult'.
+
+    Returns:
+        Chroma: A Chroma vector store containing the vectorized examples.
+    """
+    # Prepare examples for vectorization
+    to_vectorize = [' '.join(example.values()) for example in examples]
+    
+    # Initialize OpenAI embeddings
+    embeddings = OpenAIEmbeddings()
+    
+    # Create Chroma vector store
+    vectorstore = Chroma.from_texts(to_vectorize, embeddings, metadatas=examples)
+    return vectorstore
+
+
+class CustomExampleSelector(SemanticSimilarityExampleSelector):
+    """
+    Custom example selector using semantic similarity to find the closest examples for the query.
+
+    Attributes:
+        similarity_threshold (float): The minimum similarity score to consider an example relevant.
+    """
+    
+    similarity_threshold: float = 0.4
+
+    def select_examples(self, input_variables: Dict[str, str]) -> List[Dict[str, str]]:
+        """
+        Selects examples based on their semantic similarity to the input query.
+
+        Args:
+            input_variables (Dict[str, str]): The input query in the form of a dictionary with variable names and values.
+
+        Returns:
+            List[Dict[str, str]]: A list of the most relevant examples matching the input query.
+        """
         query = ' '.join(input_variables.values())
         # Get the k most similar examples
         examples_with_scores = self.vectorstore.similarity_search_with_relevance_scores(query, k=self.k)
+        
         # Filter examples based on the similarity threshold
         filtered_examples = [
             example for example, score in examples_with_scores
             if score >= self.similarity_threshold
         ]
-        # Extract the examples (without scores) to return
+        
         return [example.metadata for example in filtered_examples]
 
-example_selector = CustomSemanticSimilarityExampleSelector(
-    vectorstore=vectorstore,
-    k=1,
-    similarity_threshold=0.8
-)
 
-example_prompt = ChatPromptTemplate(
-    messages=['''
-    Question: {Question}
-    SQL Query: {SQLQuery}
-    SQL Result: {SQLResult}
-    '''],
-)
+def create_fs_prompt(vectorstore: Chroma) -> FewShotChatMessagePromptTemplate:
+    """
+    Creates a few-shot chat message prompt template using semantic similarity example selection.
 
-few_shot_prompt = FewShotChatMessagePromptTemplate(
-    input_variables=['question', 'query', 'result'], # These are passed to example_selector
-    example_selector=example_selector,
-    example_prompt=example_prompt,
-)
+    Args:
+        vectorstore (Chroma): The vector store used for selecting relevant examples.
 
-prefix = SystemMessage(content='''
-        You are an expert SQL assistant.
-        Only use the above examples as guidance or when you find user question that matches OVER 90%, otherwise prioritize over answering the user's question accurately on your own.
-        These examples are just meant to support you when you are facing complex questions, which have already been provided with solutions in guidance.
-        If you find user question that matches perfectly to any of the above examples, then you should use the result of the example since it is 100% correct answer that has been manually written by human.
-        If you face INSERT, UPDATE or DELETE requests, your answers should be really simple and insightful. Just answer users using the exact "SQL Result" provided.
-        Since you are just an assistant, you are NOT allowed to provide any further instructions to users. You just tell them whether their request was valid and successfully executed or not.
-        If the request failed, you should concisely and clearly explain ONLY the ONE MAIN problem. It is crucial for you to avoid lengthy responses and confusion for users.
-        You are NOT allowed to tell user to execute the query in MySQL environment or anything else. The query has already been executed before that, your only job is to tell if the request was successfully executed.
-''')
+    Returns:
+        FewShotChatMessagePromptTemplate: A few-shot prompt template that selects examples and prepares the final prompt.
+    """
+    # Load the example prompt from external file
+    example_prompt_message = load_message_from_file(f'{DAtA_DIR}/example_prompt.txt')
 
-final_prompt = ChatPromptTemplate.from_messages(
-    [
-        few_shot_prompt,
-        prefix,
-        ('user', '''
-        Now, Given the following user question, corresponding SQL query, and SQL result, answer the user question.
-                            
-        Question: {question}
-        SQL Query: {query}
-        SQL Result: {result}
+    # Define the example prompt template
+    example_prompt = ChatPromptTemplate(
+        messages=[example_prompt_message],
+    )
+    
+    # Create an instance of the custom semantic similarity example selector
+    example_selector = CustomExampleSelector(vectorstore=vectorstore, 
+                                             k=2,
+                                             input_keys=['question'])
+    
+    # Return a few-shot prompt template
+    return FewShotChatMessagePromptTemplate(
+        input_variables=['question', 'query', 'result'],  # Variables passed to example_selector
+        example_selector=example_selector,
+        example_prompt=example_prompt,
+    )
 
-        Please provide concise answer to avoid confusion to users and lengthy responses.
-''')
-    ]
-)
+
+def create_final_prompt(few_shot_prompt: FewShotChatMessagePromptTemplate) -> ChatPromptTemplate:
+    """
+    Creates the final prompt template by combining a few-shot prompt and system message.
+
+    Args:
+        few_shot_prompt (FewShotChatMessagePromptTemplate): The few-shot prompt template for providing examples.
+
+    Returns:
+        ChatPromptTemplate: The final prompt template ready to be used for processing queries.
+    """
+    # Load the prefix and user messages from external files
+    prefix_message = load_message_from_file(f'{DAtA_DIR}/prefix.txt')
+    user_message = load_message_from_file(f'{DAtA_DIR}/user_message.txt')
+
+    # Define the system message for the assistant's behavior
+    prefix = SystemMessage(content=prefix_message)
+
+    # Combine few-shot prompt and system message into the final prompt
+    return ChatPromptTemplate.from_messages(
+        [
+            few_shot_prompt,
+            prefix,
+            ('user', user_message)
+        ]
+    )
+
+
+def get_final_prompt() -> ChatPromptTemplate:
+    """
+    Entry point to create and return the final prompt template.
+
+    Returns:
+        ChatPromptTemplate: The final prompt template that can be used for processing user queries.
+    """
+    examples = load_json_from_file(f'{DAtA_DIR}/examples.json')
+    
+    # Create vectorstore from examples
+    vectorstore = create_vectorstore(examples)
+    
+    # Create few-shot prompt template
+    few_shot_prompt = create_fs_prompt(vectorstore)
+    
+    # Return the final prompt template
+    return create_final_prompt(few_shot_prompt)
