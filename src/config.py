905879d48121ddeabbbfd15a131afd98624ae6aa
huy <htang35@mylangara.ca>
2024-11-29 22:54:36 -0800

    Remove printing

--- src/config.py
+++ src/config.py
@@ -2,7 +2,6 @@ import os
 from pathlib import Path
 
 from dotenv import load_dotenv
-from loguru import logger
 
 
 # Load environment variables from .env file
@@ -18,7 +17,6 @@ DB_NAME = os.getenv("DB_NAME")
 
 # Paths
 PROJ_ROOT = Path(__file__).resolve().parents[1]
-logger.info(f"Project root path is: {PROJ_ROOT}")
 
 SRC_DIR = PROJ_ROOT / "src"
 LOGS_DIR = PROJ_ROOT / "logs"
@@ -28,18 +26,3 @@ STYLES_DIR = SRC_DIR / "styles"
 
 # Ensure the logs directory exists
 LOGS_DIR.mkdir(parents=True, exist_ok=True)
-
-# If tqdm is installed, configure loguru to integrate with tqdm for progress bars
-try:
-    from tqdm import tqdm
-
-    # Check if the default handler exists before removing it
-    if 0 in logger._core.handlers:
-        # Remove the default loguru handler
-        logger.remove(0)
-
-    # Add a new handler to use tqdm"s write function for log messages, ensuring compatibility with progress bars
-    logger.add(lambda msg: tqdm.write(msg, end=""), colorize=True)
-    logger.success("tqdm integration enabled for logging.")
-except ModuleNotFoundError:
-    logger.warning("tqdm is not installed. Proceeding without tqdm integration.")
