8efe66d9406ca7f29c92f127efa5d21ec35bf473
huy <htang35@mylangara.ca>
2024-12-04 21:09:34 -0800

    Added error handling and change docstring

--- src/agent.py
+++ src/agent.py
@@ -2,34 +2,43 @@ from langchain.agents.agent import AgentExecutor
 from langchain_community.agent_toolkits import create_sql_agent
 from langchain_community.utilities import SQLDatabase
 from langchain_openai import ChatOpenAI
+from loguru import logger
 
+from custom_exceptions import AgentError
 from FSL_prompt import get_final_prompt
 
 
 def create_agent(llm: ChatOpenAI, db: SQLDatabase) -> AgentExecutor:
     """
-    Create an agent that executes SQL queries using a language model.
-
-    This function configures a SQL agent that connects a language model to a SQL database
-    allowing the model to autonomously generate and execute SQL queries based on user input.
+    Initializes an agent to process natural language inputs and execute SQL queries.
 
     Args:
-        llm (ChatOpenAI): The language model agent from OpenAI.
-        db (SQLDatabase): The SQL database configuration used by the agent.
+        llm (ChatOpenAI): Language model for query generation.
+        db (SQLDatabase): Database connection for query execution.
 
     Returns:
-        AgentExecutor: An executable agent that can run SQL queries.
+        AgentExecutor: Configured agent for SQL operations.
+
+    Raises:
+        AgentError: If agent creation fails.
     """
     # Generate the final prompt from a predefined function
     full_prompt = get_final_prompt()
 
-    # Create the SQL agent with verbose output to trace operations
-    agent_executor = create_sql_agent(
-        llm,
-        db=db,
-        prompt=full_prompt,
-        agent_type="openai-tools",
-        verbose=True
-    )
+    try:
+        # Create the SQL agent with verbose output to trace operations
+        agent_executor = create_sql_agent(
+            llm,
+            db=db,
+            prompt=full_prompt,
+            agent_type="openai-tools",
+            verbose=True
+        )
+    except Exception as agent_error:
+        logger.error(f"Failed to create agent. Details:\n{agent_error}")
+        raise AgentError(
+            "If you are seeing this message, it means that there has been a technical problem from our side. " +
+            "Please contact us so that we can resolve the issue as soon as possible. We apologize for such inconvenience."
+        ) from agent_error
 
     return agent_executor
