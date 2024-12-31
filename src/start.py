a3a262d8b922ce66ec2759ca4c174883d2fcec17
huy <htang35@mylangara.ca>
2024-12-30 17:22:50 -0800

    Utilized scripting for running the whole app

---
+++ src/start.py
@@ -0,0 +1,35 @@
+import sys
+import subprocess
+import signal
+
+
+FASTAPI = "src/API/server.py"
+STREAMLIT = "src/UI/app.py"
+
+processes = []
+
+def shutdown():
+    """Terminate all subprocesses."""
+    print("Shutting down...")
+    for process in processes:
+        process.terminate()
+    sys.exit(0)
+
+def main():
+    """Start the full application"""
+    try:
+        backend = subprocess.Popen([sys.executable, FASTAPI])
+        frontend = subprocess.Popen(["streamlit", "run", STREAMLIT])
+        processes.extend([backend, frontend])
+    except Exception as e:
+        print(e)
+        sys.exit(1)
+
+    signal.signal(signal.SIGINT, shutdown)
+    signal.signal(signal.SIGTERM, shutdown)
+
+    backend.wait()
+    frontend.wait()
+
+if __name__ == "__main__":
+    main()
