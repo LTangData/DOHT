3937eda9577e7acb224860ceab23320165ee2dc8
huy <htang35@mylangara.ca>
2025-01-07 15:26:31 -0800

    Added new attribute for DatabaseConnectionRequest model + Updated documentation

--- src/API/models.py
+++ src/API/models.py
@@ -3,35 +3,39 @@ from pydantic import BaseModel
 
 class DatabaseConnectionRequest(BaseModel):
     """
-    Request model for establishing a database connection.
+    Data model representing the credentials and configuration for establishing a database connection.
 
     Attributes:
-        user (str): The username for authenticating the database connection.
-        password (str): The password for authenticating the database connection.
+        dbms (str): The type of database management system (e.g., "MySQL", "PostgreSQL", "MongoDB").
+        user (str): The username used for authenticating the database connection.
+        password (str): The password used for authenticating the database connection.
         host (str): The hostname or IP address of the database server.
-        port (int): The port number to connect to the database (default is 3306).
+        port (int): The port number to connect to the database (e.g., 3306 for MySQL, 5432 for PostgreSQL).
         database (str): The name of the database to connect to.
+        db_schema (str | None): The schema to use within the database (specifically for PostgresQL). Defaults to None.
     """
+    dbms: str
     user: str
     password: str
     host: str
-    port: int = 3306
+    port: int
     database: str
+    db_schema: str | None = None
 
 class QueryRequest(BaseModel):
     """
-    Request model for sending a query.
+    Data model representing a request to process a query or question.
 
     Attributes:
-        input (str): The query or question to be processed.
+        input (str): The query or question provided by the user for processing.
     """
     input: str
 
 class QueryResponse(BaseModel):
     """
-    Response model for returning query results.
+    Data model representing the response generated for a given query.
 
     Attributes:
-        output (str): The generated response or answer to the query.
+        output (str): The result or answer generated in response to the user's query.
     """
     output: str
